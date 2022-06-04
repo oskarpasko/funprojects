11f6ef5096b9012d35da59fa86641a5e4b4fa362
Oskar <oskarpasko@gmail.com>
2022-06-04 17:20:08 +0200

    Start Project

---
+++ Bizzare3T/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(" X " + " | " + " O " + " | " + " X ");
+        System.out.println("----|-----|----");
+        System.out.println(" X " + " | " + " X " + " | " + " X ");
+        System.out.println("----|-----|----");
+        System.out.println(" X " + " | " + " O " + " | " + " O ");
+
+    }
+}
