50816063b6bcde50458bead730fcc8106b3fc9c8
Oskar <oskarpasko@gmail.com>
2022-06-04 19:47:27 +0200

    Transfer code with game mechanism from Main to Game class

--- Bizzare3T/src/com/company/Game.java
+++ Bizzare3T/src/com/company/Game.java
@@ -1,4 +1,54 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Game {
+
+    //create objects of outputs and Scanner
+    static Output output = new Output();
+    static Scanner in = new Scanner(System.in);
+
+    //Array which is sending to game_output()
+    static String[] game = {"-", "-", "-",
+            "-", "-", "-",
+            "-", "-", "-"};
+
+    //Variables definitions
+    static int player_checker = 1;
+    static int piece = 1;
+
+    public static void game(){
+        //Outputs default board
+        output.game_output(game);
+
+        //Main game's loop
+            while (player_checker < 10) {
+            if (player_checker % 2 != 0) {
+                //Ask player which cube want to choose
+                System.out.println("Player 1, where would You like place Your piece?\n");
+
+                //player's choise
+                piece = in.nextInt();
+
+                //Add choice to the game's array
+                game[piece - 1] = "X";
+
+                //output game's board
+                output.game_output(game);
+            } else {
+                //Ask player which cube want to choose
+                System.out.println("Player 2, where would You like place Your piece?\n");
+                //player's choise
+                piece = in.nextInt();
+
+                //Add choice to the game's array
+                game[piece - 1] = "O";
+
+                //output game's board
+                output.game_output(game);
+            }
+
+            player_checker++;
+        }
+    }
 }
--- Bizzare3T/src/com/company/Main.java
+++ Bizzare3T/src/com/company/Main.java
@@ -5,54 +5,13 @@ import java.util.Scanner;
 public class Main {
 
     static Output output = new Output();
+    static Game game = new Game();
 
     public static void main(String[] args) {
 
         //outputs welcome texts
         output.welcome();
 
-        //Array which is sending to game_output()
-        String[] game = {"-", "-", "-",
-                "-", "-", "-",
-                "-", "-", "-"};
-
-        //Outputs default board
-        output.game_output(game);
-
-
-        int player_checker = 1;
-        int piece = 1;
-        Scanner in = new Scanner(System.in);
-
-        //Main game's loop
-        while (player_checker < 10) {
-            if (player_checker % 2 != 0) {
-                //Ask player which cube want to choose
-                System.out.println("Player 1, where would You like place Your piece?\n");
-                //player's choise
-                piece = in.nextInt();
-
-                //Add choice to the game's array
-                game[piece - 1] = "X";
-
-                //output game's board
-                output.game_output(game);
-            } else {
-                //Ask player which cube want to choose
-                System.out.println("Player 2, where would You like place Your piece?\n");
-                //player's choise
-                piece = in.nextInt();
-
-                //Add choice to the game's array
-                game[piece - 1] = "O";
-
-                //output game's board
-                output.game_output(game);
-            }
-
-            player_checker++;
-        }
-
-
+        game.game();
     }
 }
