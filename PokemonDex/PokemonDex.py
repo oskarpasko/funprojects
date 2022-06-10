e4bced2da7a70a80d586d81c8d0746d5bee1f57d
Oskar <oskarpasko@gmail.com>
2022-06-10 22:27:32 +0200

    Testing

--- PokemonDex/PokemonDex.py
+++ PokemonDex/PokemonDex.py
@@ -2,5 +2,10 @@ from src.Pokemon import Pokemon
 
 pokemons = [Pokemon('Torchic', 5), Pokemon('Mudkip', 5), Pokemon('Treeko', 5)]
 
-for pokemon in pokemons:
-    pokemon.show_pokemon()
+# Writes all pokemons from list
+#for pokemon in pokemons:
+#    pokemon.show_pokemon()
+
+
+# Write specific pokemon with index
+# pokemons[1].show_pokemon()
