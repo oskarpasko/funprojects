978ef3561f4d0b98e41765771f231225fd9df79b
Oskar <oskarpasko@gmail.com>
2022-06-10 23:18:52 +0200

    Testing ideas for fight

--- PokemonDex/PokemonDex.py
+++ PokemonDex/PokemonDex.py
@@ -19,5 +19,48 @@ pokemons = [Pokemon('Torchic', 5),
 # pokemons[1].show_pokemon()
 
 # random pokemon :O
-for x in range(0, 10):
-    random.choice(pokemons).show_pokemon()
+#for x in range(0, 10):
+#    random.choice(pokemons).show_pokemon()
+
+
+
+
+# Catch pokemon tests
+print("\nYou've found wild Pokemon! \n")
+random.choice(pokemons).show_pokemon()
+checker = False
+
+
+# main method of fight?
+while(checker == False):
+    print('\nChoose Your action!')
+    print('1. Catch!')
+    print('2. Run!\n')
+
+    choice = input('...')
+
+    match(choice):
+        case '1':
+            if(random.randint(0, 10) > 5):
+                print('1...')
+                if(random.randint(0, 10) > 5):
+                    print('2...')
+                    if(random.randint(0, 10) > 5):
+                        print('3...')
+                        if(random.randint(0, 10) > 5):
+                            print("You've just caught new pokemon!")
+                            checker = True
+                        else:
+                            print("That was 3!")
+                    else:
+                        print("Try again!")
+                else:
+                    print("Ugh, try again!")
+            else:
+                print("Ohhh, try again!")
+        case '2':
+            print("You've run away!")
+            checker = True
+        case _:
+            print("Wrong choice!")
+
