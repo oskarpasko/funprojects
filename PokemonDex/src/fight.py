0291fee0b41360cab0a90d36da2d19efe71f82a4
Oskar <oskarpasko@gmail.com>
2022-06-13 11:06:29 +0200

    Add method for fight mechanics

---
+++ PokemonDex/src/fight.py
@@ -0,0 +1,46 @@
+import random
+from src.wild_pokemon import WildPokemon
+
+def fight():
+    # Start Battle!!!!!
+
+    # Catch pokemon tests
+    print("\nYou've found wild Pokemon! \n")
+    appeard_pokemon = random.choice(WildPokemon.pokemons_test_route)
+    pokemon_catch_ratio = appeard_pokemon.catch_ratio
+    checker = False
+
+    appeard_pokemon.show_pokemon()
+
+    # main method of fight
+    while(checker == False):
+        print('\nChoose Your action!')
+        print('1. Catch!')
+        print('2. Run!\n')
+
+        choice = input('...')
+
+        match(choice):
+            case '1':
+                if(random.randint(0, 10) > pokemon_catch_ratio):
+                    print('1...')
+                    if(random.randint(0, 10) > pokemon_catch_ratio):
+                        print('2...')
+                        if(random.randint(0, 10) > pokemon_catch_ratio):
+                            print('3...')
+                            if(random.randint(0, 10) > pokemon_catch_ratio):
+                                print("You've just caught new pokemon!")
+                                checker = True
+                            else:
+                                print("That was 3!")
+                        else:
+                            print("Try again!")
+                    else:
+                        print("Ugh, try again!")
+                else:
+                    print("Ohhh, try again!")
+            case '2':
+                print("You've run away!")
+                checker = True
+            case _:
+                print("Wrong choice!")
