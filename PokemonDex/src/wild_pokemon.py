b7726f9177d44cf1eeddc7dbe3994f97e4bde5df
Oskar <oskarpasko@gmail.com>
2022-06-13 12:05:58 +0200

    Update

--- PokemonDex/src/wild_pokemon.py
+++ PokemonDex/src/wild_pokemon.py
@@ -3,10 +3,12 @@ from src.Pokemon import Pokemon
 # list of pokemons
 
 class WildPokemon:
-    pokemons_test_route = [Pokemon('Torchic', 5, 5),
+    pokemons_test_route = [
+                Pokemon('Torchic', 5, 5),
                 Pokemon('Mudkip', 5, 5),
                 Pokemon('Treeko', 5, 5),
                 Pokemon("Poochyena", 2, 3),
                 Pokemon("Wurmple", 2, 0),
                 Pokemon("Wurmple", 3, 1),
-                Pokemon('Wurmple', 4, 2)]
+                Pokemon('Wurmple', 4, 2)
+                ]
