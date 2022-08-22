071ddc3403e7f78b9abeaa00cb0e61ab1ea2087d
Oskar <oskarpasko@gmail.com>
2022-08-22 15:19:06 +0200

    Add sterring and colliders

--- MealDriver/Assets/Driver.cs
+++ MealDriver/Assets/Driver.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float steerSpeed = 250f;
+
     void Start()
     {
-        Debug.Log("I will be your Driver!");
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
