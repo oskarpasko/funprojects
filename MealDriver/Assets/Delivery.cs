ee4c5afbe2ebf2584a9aa96178870b0604279d93
Oskar <oskarpasko@gmail.com>
2022-08-22 23:10:32 +0200

    Add more logic to packages

--- MealDriver/Assets/Delivery.cs
+++ MealDriver/Assets/Delivery.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+
+    [SerializeField] float destroyDelay = 0.5f;
+    bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch that hurts!");
@@ -11,14 +24,19 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Package")
+        if(other.tag == "Package" && hasPackage == false)
         {
             Debug.Log("Package picked up!");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
         }
 
-        if(other.tag == "Customer")
+        if(other.tag == "Customer" && hasPackage == true)
         {
             Debug.Log("Package was deliverd!");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
