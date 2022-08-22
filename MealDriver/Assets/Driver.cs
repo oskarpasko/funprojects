8b9e17cb6246bc96f478e6fbf56c1b5f3059fc7c
Oskar <oskarpasko@gmail.com>
2022-08-22 23:26:28 +0200

    Add boosts and Bumps

--- MealDriver/Assets/Driver.cs
+++ MealDriver/Assets/Driver.cs
@@ -6,6 +6,8 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 15f;
     [SerializeField] float steerSpeed = 250f;
+    [SerializeField] float slowSpeed = 14f;
+    [SerializeField] float boostSpeed = 30f;
 
     void Start()
     {
@@ -19,5 +21,18 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+            moveSpeed = slowSpeed;
     }
 }
