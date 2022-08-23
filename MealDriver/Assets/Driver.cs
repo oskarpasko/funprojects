9eebde29f53ef799b4eebb541d4db61c179ebf78
Oskar <oskarpasko@gmail.com>
2022-08-23 23:07:37 +0200

    Clean some code

--- MealDriver/Assets/Driver.cs
+++ MealDriver/Assets/Driver.cs
@@ -21,8 +21,6 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
-
-        
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
