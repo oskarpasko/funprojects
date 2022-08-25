ab6dca531ed25b0e0e62cfef96e23c0cf36e3567
Oskar <oskarpasko@gmail.com>
2022-08-25 14:19:04 +0200

    Add Finish Line and message with collision on your head

--- Snow Flips/Assets/Scripts/FinishLine.cs
+++ Snow Flips/Assets/Scripts/FinishLine.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnTriggerEnter2D(Collider2D other) 
+    {  
+        if(other.tag == "Player")
+        {
+            Debug.Log("You finished this level!"); 
+        }
     }
 }
