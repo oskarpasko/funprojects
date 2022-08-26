759b3b9fdc6ff93be6693f08972be3b8df24f349
Oskar <oskarpasko@gmail.com>
2022-08-26 13:54:18 +0200

    Add Particles to our game and tune some space

--- Snow Flips/Assets/Scripts/CrashDetector.cs
+++ Snow Flips/Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 2f;
+    [SerializeField] ParticleSystem crashEffect;
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
-            Debug.Log("What a shame!");
+            crashEffect.Play();
+            Invoke("ReloadScene", loadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+
     }
 }
--- Snow Flips/Assets/Scripts/FinishLine.cs
+++ Snow Flips/Assets/Scripts/FinishLine.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+[SerializeField] float loadDelay = 2f;
+[SerializeField] ParticleSystem finishEffect;
+
     void OnTriggerEnter2D(Collider2D other) 
     {  
         if(other.tag == "Player")
         {
-            Debug.Log("You finished this level!"); 
+            finishEffect.Play();
+            Invoke("ReloadScene", loadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+
     }
 }
