c8015458cf89a4ff9ac076c70c057efb98559024
Oskar <>
2022-10-27 19:44:10 +0200

    Add algorithm with exponentation

--- Mathmatica/src/algorithms.py
+++ Mathmatica/src/algorithms.py
@@ -1,9 +1,16 @@
+from .settings import Colors, print_colored
+
 def exponentation():
-    base = int(input('Enter base: '))
+    base = int(input('Enter base: ')) 
     exponent = int(input('Enter exponent: '))
     result = 1
 
-    for x in range (0, exponent):
-        result *= base
-
-    print(result)
+    if(exponent >= 0):
+        for x in range (0, exponent):
+            result *= base
+        print(f"{base}^{exponent} = {result}")
+    else:
+        exponent *= -1
+        for x in range (0, exponent):
+            result *= base
+        print(f"{base}^{-1*exponent} = {1/result}")
