e74765a61980e520d5425b29aa5aacd94c7cc76e
Oskar <>
2022-10-27 18:37:20 +0200

    Create menu.py

---
+++ Mathmatica/src/menu.py
@@ -0,0 +1,6 @@
+from .settings import Colors
+
+def display_menu():
+    print(f"{Colors.PURPLE}1. Exponentiation{Colors.END}")
+    print(f"{Colors.PURPLE}2. Errors{Colors.END}")
+    print(f"{Colors.PURPLE}3. Exit{Colors.END}")
