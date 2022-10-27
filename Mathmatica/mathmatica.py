2f3b5bf5c34cc7ced515c4e8926d21139a3c6277
Oskar <>
2022-10-27 18:37:24 +0200

    Update mathmatica.py

--- Mathmatica/mathmatica.py
+++ Mathmatica/mathmatica.py
@@ -1,5 +1,25 @@
-from src.settings import text_display
+from src.algorithms import *
+from src.errors import errors
+from src.menu import display_menu
 
 print(' =================================== ')
 print('        Welcome in Mathmatica')
 print(' =================================== ')
+
+print('\n')
+
+while(True):
+
+    display_menu()
+
+    choice = input("Choose option: ")
+
+    match(choice):
+        case '1':
+            exponentation()
+        case '2':
+            errors()
+        case '3':
+            exit()
+        case _:
+            print('Error 890!')
