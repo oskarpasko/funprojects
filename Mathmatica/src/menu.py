417d7434cd60810e52da8b33b96811afd79fe7d3
Oskar <>
2022-10-29 15:32:32 +0200

    add new algorithm to menu

--- Mathmatica/src/menu.py
+++ Mathmatica/src/menu.py
@@ -3,5 +3,6 @@ from .settings import Colors
 def display_menu():
     print(f"{Colors.PURPLE}1. Exponentiation{Colors.END}")
     print(f"{Colors.PURPLE}2. Factorial{Colors.END}")
-    print(f"{Colors.PURPLE}3. Errors{Colors.END}")
-    print(f"{Colors.PURPLE}4. Exit{Colors.END}")
+    print(f"{Colors.PURPLE}3. Newton Symbol{Colors.END}")
+    print(f"{Colors.PURPLE}4. Errors{Colors.END}")
+    print(f"{Colors.PURPLE}5. Exit{Colors.END}")
