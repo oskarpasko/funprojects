fba9a69378b8d20ee8e613cba4fa97fb0b6472df
Oskar <>
2022-10-29 15:32:48 +0200

    add new algorithm to matmathica

--- Mathmatica/mathmatica.py
+++ Mathmatica/mathmatica.py
@@ -19,11 +19,15 @@ while(True):
         case '1':
             exponentation()
         case '2':
-            N = int(input(f"{Colors.CYAN}Podaj n!: {Colors.END}"))
-            print(f"{Colors.LIGHT_GREEN}{factorial(N)}{Colors.END}")
+            N = int(input(f"{Colors.CYAN}Podaj n: {Colors.END}"))
+            print(f"{Colors.LIGHT_GREEN}{N}! = {factorial(N)}{Colors.END}")
         case '3':
-            errors()
+            N = int(input(f"{Colors.CYAN}Podaj n: {Colors.END}"))
+            K = int(input(f"{Colors.CYAN}Podaj k: {Colors.END}"))
+            print(f"{Colors.LIGHT_GREEN}{N}! / {K}!({N} - {K})! = {newton_symbol(N, K)}{Colors.END}")
         case '4':
+            errors()
+        case '5':
             exit()
         case _:
             print('Error 890!')
