f746eb0d2eda864b92a33f6b756b07e130a4a480
Oskar <>
2022-10-30 16:29:30 +0100

    Update menu.py

--- Mathmatica/src/menu.py
+++ Mathmatica/src/menu.py
@@ -4,5 +4,6 @@ def display_menu():
     print(f"{Colors.PURPLE}1. Exponentiation{Colors.END}")
     print(f"{Colors.PURPLE}2. Factorial{Colors.END}")
     print(f"{Colors.PURPLE}3. Newton Symbol{Colors.END}")
-    print(f"{Colors.PURPLE}4. Errors{Colors.END}")
-    print(f"{Colors.PURPLE}5. Exit{Colors.END}")
+    print(f"{Colors.PURPLE}4. Quadratic Function{Colors.END}")
+    print(f"{Colors.PURPLE}5. Errors{Colors.END}")
+    print(f"{Colors.PURPLE}6. Exit{Colors.END}")
