690fc6beb60c1052de703d07ae26c341cbb76cf1
Oskar <>
2022-11-03 21:37:29 +0100

    add file_reader to main projects file

--- PythonTemplates/main.py
+++ PythonTemplates/main.py
@@ -1,6 +1,12 @@
 from src.settings import Colors
 from src.menu import menu
+from src.functions import *
 
 print(f"{Colors.YELLOW}Some templates in Python. Rather for devs than users :){Colors.END}")
 
 menu()
+choice = input(f'{Colors.CYAN}Choose option: {Colors.END}')
+
+match(choice):
+    case '1':
+        file_reader()
