d09c8459b8de6f2d3521c0febcd998da7ab85eff
Oskar <>
2022-11-03 19:25:57 +0100

    Update

--- PythonTemplates/main.py
+++ PythonTemplates/main.py
@@ -1,3 +1,6 @@
 from src.settings import Colors
+from src.menu import menu
 
 print(f"{Colors.YELLOW}Some templates in Python. Rather for devs than users :){Colors.END}")
+
+menu()
---
+++ PythonTemplates/src/menu.py
@@ -0,0 +1,7 @@
+from .settings import Colors
+
+def menu():
+    print(f"{Colors.BLUE}1.{Colors.END}")
+
+if __name__ == '__main__':
+    menu()
