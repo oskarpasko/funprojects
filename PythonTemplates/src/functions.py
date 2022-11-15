072821cf324e1791bd2825136738f10fa141bf39
Oskar <>
2022-11-15 14:45:22 +0100

    Add some comments

--- PythonTemplates/src/functions.py
+++ PythonTemplates/src/functions.py
@@ -4,8 +4,17 @@ def file_reader():
         contents = file_object.read()
     print(contents)
 
+# function with try except block
 def try_except(first, second):
+
+    # block where the error could be
     try:
+        # example: 5/0
         return first / second
+    # block with Error Division
     except ZeroDivisionError:
         return "Nie dzielimy przez 0!"
+    # else:
+    #   operations
+    # finally:
+    #   operations
