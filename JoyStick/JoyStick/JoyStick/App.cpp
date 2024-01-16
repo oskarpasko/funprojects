87c8a6ee8de3c951510a555f8852d63a1c3e3528
Oskar <oskarpasko@gmail.com>
2024-01-16 21:11:06 +0100

    app version 2.0

--- JoyStick/JoyStick/JoyStick/App.cpp
+++ JoyStick/JoyStick/JoyStick/App.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <stdio.h>
 #include <conio.h>
+#include <cstdlib>
+#include <ctime>
+
 #include "JoyStick.h"
 
 using namespace std;
 
+// funkcja wypisuj¹ca ca³¹ tablice
 void arrayOutput(string array[][10])
 {
 	int i, j;
@@ -16,12 +20,50 @@ void arrayOutput(string array[][10])
 		}
 		cout << endl;
 	}
+	cout << endl;
+}
+
+// funkcja losuj¹ca liczbê z przedzia³o [0, 10]
+int randomNumber()
+{
+	return rand() % 10;
+}
+
+void game()
+{
+
+}
+
+void welcome()
+{
+	cout << "Witamy w symulatorze JoySticka."
+		"Twoim zadaniem jest dojsc do wykrzyknika na planszy."
+		" JoyStickiem poruszasz sie za pomoca przyciskow W (gora), A (lewo), S (dol), D (prawo)."
+		" Jesli chcesz zaczac wcisnij przycisk '1'. Powodzenia!" << endl;
 }
 
 int main()
 {
-    string arr[10][10] = {
-        {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+	welcome();
+
+	while (true)
+	{
+		int check;
+		cin >> check;
+		if (check == 1) break;
+	}
+
+	system("CLS");
+
+	srand(time(NULL));
+
+	// konstruktor naszego joystick
+	// przypisanie losowych koordynatów X oraz Y
+	JoyStick* js = new JoyStick(randomNumber(), randomNumber());
+
+	// zmienna z surow¹ tablic¹
+	string arr[10][10] = {
+		{"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
 		{"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
 		{"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
 		{"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
@@ -31,38 +73,43 @@ int main()
 		{"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
 		{"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
 		{"-", "-", "-", "-", "-", "-", "-", "-", "-", "!"},
-            };
+	};
 
-	JoyStick js;
+	// ustawienie w wylosowanych koordynatach joysticka
+	js->setBoard(arr);
 
-	js.setBoard(arr);
-
+	// pêtla poruszaj¹ca siê po planszy
 	while (true)
 	{
+		// wypisanie planszy
 		arrayOutput(arr);
 
+		// pobranie od u¿ytkownika wciœniêtego klawisza
 		char choice = _getch();
 
 		switch (choice)
 		{
-		case 'w':
-			js.getCords(arr);
-			js.moveUp(arr);
+		case 'w': // poruszeniesiê w górê planszy
+			js->getCords(arr);
+			js->moveUp(arr);
 			break;
-		case 'd':
-			js.getCords(arr);
-			js.moveRight(arr);
+		case 'd': // poruszenie siê w praw¹ stronê planszy
+			js->getCords(arr);
+			js->moveRight(arr);
 			break;
-		case 's':
-			js.getCords(arr);
-			js.moveDown(arr);
+		case 's': // poruszenie siê w dó³ planszy
+			js->getCords(arr);
+			js->moveDown(arr);
 			break;
-		case 'a':
-			js.getCords(arr);
-			js.moveLeft(arr);
+		case 'a': // poruszenie siê w lew¹ stronê planszy
+			js->getCords(arr);
+			js->moveLeft(arr);
 			break;
 		case 'q':
+			// wywo³anie destruktora
+			delete js;
 			exit(0);
 		}
+		system("CLS");
 	}
 }
--- JoyStick/JoyStick/JoyStick/JoyStick.h
+++ JoyStick/JoyStick/JoyStick/JoyStick.h
@@ -1,20 +1,61 @@
 #include <iostream>
 using namespace std;
 
-
-class JoyStick
+class Device
 {
-private:
-	 int x = 0;
-	 int y = 0;
+protected:
+	// sk³adowe z koordynatami joysticka
+	int x;
+	int y;
 
+	// sk³adowa z wielkoœci¹ planszy jak¹ obs³uguje joystick
+	string goodBye = "Dziekujemy za skorzystanie z naszego programu!" ;
+};
+
+class JoyStick : Device
+{
 public:
+	/*
+	
+	W aplikacji u¿ywany jest konstruktor z parametrami,
+	dlatego pozosta³e konstruktory s¹ zakomentowane
+	
+	*/
 
+	//konstruktor domyœlny
+	/*JoyStick()
+	{
+		this->x = 0;
+		this->y = 0;
+	}*/
+
+	// konstruktor z parametrami
+	JoyStick(int xx, int yy)
+	{
+		this->x = xx;
+		this->y = yy;
+	}
+
+	//konstruktor kopiuj¹cy
+	/*JoyStick(JoyStick& copyx, JoyStick& copyy, JoyStick& copys)
+	{
+		this->x = copyx.x;
+		this->y = copyy.y;
+	}*/
+
+	// destruktor
+	~JoyStick()
+	{
+		cout << this->goodBye;
+	}
+
+	// metoda ustawiaj¹ca nasz joystick na wybranych koordynatach
 	void setBoard(string board[][10])
 	{
 		board[x][y] = "+";
 	}
 
+	// metoda pobieraj¹ca nasz¹ aktualn¹ pozycjê
 	void getCords(string board[][10])
 	{
 		int i, j;
@@ -28,14 +69,20 @@ public:
 					y = j;
 				}
 			}
-			cout << endl;
 		}
 	}
 
+	//metody poruszaj¹ce siê odpowiednio:
+	// w prawo
+	// w lewo
+	// w dó³
+	// w górê
 	void moveRight(string board[][10])
 	{
 		if (board[x][y + 1] == "!")
 		{
+			board[x][y] = "-";
+			board[x][y + 1] = "+";
 			cout << "Dotarles do celu! Gratulacje!" << endl;
 			exit(0);
 		}
@@ -51,6 +98,8 @@ public:
 	{
 		if (board[x][y - 1] == "!")
 		{
+			board[x][y] = "-";
+			board[x][y - 1] = "+";
 			cout << "Dotarles do celu! Gratulacje!" << endl;
 			exit(0);
 		}
@@ -66,6 +115,8 @@ public:
 	{
 		if (board[x + 1][y] == "!")
 		{
+			board[x][y] = "-";
+			board[x + 1][y] = "+";
 			cout << "Dotarles do celu! Gratulacje!" << endl;
 			exit(0);
 		}
@@ -81,6 +132,8 @@ public:
 	{
 		if (board[x-1][y] == "!")
 		{
+			board[x][y] = "-";
+			board[x - 1][y] = "+";
 			cout << "Dotarles do celu! Gratulacje!" << endl;
 			exit(0);
 		}
